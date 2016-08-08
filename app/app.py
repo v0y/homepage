041b03ecc2c1c7c46270b6181a2e3f1dc7b7ecd0
voy <voy@lolwtf.pl>
2016-08-08 15:11:52 +0200

    Add buddyzm page

--- app/app.py
+++ app/app.py
@@ -12,5 +12,10 @@ def hello_world():
     return render_template('index.html')
 
 
+@application.route('/buddyzm')
+def buddyzm():
+    return render_template('buddyzm.html')
+
+
 if __name__ == '__main__':
     application.run()
